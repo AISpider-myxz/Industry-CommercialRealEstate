64ff7200fe0ca8402e8c83264576ca76b8ef1f54
梦游 <126550160+MengYouXz@users.noreply.github.com>
2024-05-07 15:21:31 +0800

    1

--- AISpider/main.py
+++ AISpider/main.py
@@ -10,4 +10,4 @@ os.chdir(current_dir)# 跳转到当前文件所在目录
 
 # execute(['celery','-A','celery_app','worker','-l','info','-n','celery@get_detial'])
 # execute(['celery -A celery_app worker -l info -n celery@get_detial'])
-execute(['scrapy', 'crawl', 'demo2'])
+execute(['scrapy', 'crawl', 'commercialrealestate','-a','category=for-lease'])
